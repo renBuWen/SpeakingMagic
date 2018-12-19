85666befc017950e9e227d0410e974dbedb8137c
Aery <31158107+KillerAery@users.noreply.github.com>
2018-12-19 12:22:11 +0800

    加入辅助工具黑板类

--- Assets/Script/Item/ItemDataTypes.cs
+++ Assets/Script/Item/ItemDataTypes.cs
@@ -1,34 +1,40 @@
 ﻿
-
+//一种物品的属性
 struct ItemType{
     string name;
+    string type;
     int id;
     int maxNum;
     int width;
     int height;
+    TBlackBoard blackBoard;
 }
 
-struct Item
-{
-    int id;
-    int num;
-}
-
-struct ItemGrid
-{
-    Item item;
-}
-
+//一种容器的属性
 struct ContainerType
 {
     string name;
     int id;
     int capacityX;
     int capacityY;
+    TBlackBoard blackBoard;
 }
 
-struct Container
+//物品格
+class ItemGrid{
+    public Item item;
+}
+
+//物品数据
+class Item
 {
-    int id;
-    ItemGrid[][] girds;
+    public int id;
+    public int num;
+}
+
+//容器数据
+class Container
+{
+    public int id;
+    public ItemGrid[][] girds;
 }
